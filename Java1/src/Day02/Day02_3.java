50ca4bfa497719ebb0c3e6f302243deb55c306bd
504 <504@DESKTOP-S5HG3D6>
2022-03-02 12:24:52 +0900

    123

--- Java1/src/Day02/Day02_3.java
+++ Java1/src/Day02/Day02_3.java
@@ -13,16 +13,21 @@ public class Day02_3 {
 		 * 	[출력조건]
 		 * 		실수령액 출력
 		 */
+		
+		//* 입력 -> 저장 -> 계산 -> 출력
+		// 1. 입력 객체 만든다.
+		// 2. 입력받은 값을 변수에 저장한다. [ 입력객체는 입력 받을대마다 기존 데이터가 사라진다.]
+		// 3. 변수를 연산 한다.
+		// 4. 출력한다.
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("기본급을 입력하세요 :"); String value1 = sc.next();
-		int num1 = Integer.parseInt(value1);
-		System.out.print("수당을 입력하세요 :"); String value2 = sc.next();
-		int num2 = Integer.parseInt(value2);
+		System.out.print("기본급을 입력하세요 :"); int value1 = sc.nextInt();
+		System.out.print("수당을 입력하세요 :"); int value2 = sc.nextInt();
+
 		
-		double sum = num1 + num2 - (num1 * 0.1);
+		double sum = value1 + value2 - (value1 * 0.1);
 		
-		System.out.println(sum);
+		System.out.println("회원님의 실 수령액 : " + sum);
 		
 		
 		
@@ -58,7 +63,7 @@ public class Day02_3 {
 		
 		int val4 = (int)(num3/100);
 		
-		System.out.println("10만원: " + val1 + "장\n1만원: " + val2 + "장\n1천원: " + val3 + "장\n1백원: " + "개" );
+		System.out.println("10만원: " + val1 + "장\n1만원: " + val2 + "장\n1천원: " + val3 + "장\n1백원: " + val4+ "개" );
 		
 	}
 }
